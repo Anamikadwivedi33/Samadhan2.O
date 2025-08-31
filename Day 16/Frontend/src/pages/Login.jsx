a72400195295b899ca1b7d2ea518074e743cbed7
Sadhvi Pandey <122966840+Sadhvi26@users.noreply.github.com>
2025-08-31 10:39:45 +0530

    Create Login.jsx

---
+++ Day 16/Frontend/src/pages/Login.jsx
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+import { loginUser } from "../services/api";
+
+function Login() {
+  const [form, setForm] = useState({ email: "", password: "" });
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const res = await loginUser(form);
+    alert(res.data.message);
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <h2>Login</h2>
+      <input
+        type="email"
+        placeholder="Email"
+        value={form.email}
+        onChange={(e) => setForm({ ...form, email: e.target.value })}
+      />
+      <input
+        type="password"
+        placeholder="Password"
+        value={form.password}
+        onChange={(e) => setForm({ ...form, password: e.target.value })}
+      />
+      <button type="submit">Login</button>
+    </form>
+  );
+}
+
+export default Login;
