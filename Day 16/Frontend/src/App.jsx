05829cbe6a6cd35415a1d6b915178b8d325b031d
Sadhvi Pandey <122966840+Sadhvi26@users.noreply.github.com>
2025-08-31 10:31:56 +0530

    Create App.jsx

---
+++ Day 16/Frontend/src/App.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import Products from "./pages/Products";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import Cart from "./components/Cart";
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/cart" element={<Cart />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
